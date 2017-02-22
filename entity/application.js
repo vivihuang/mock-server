4486cdfd54fcc93770e0f4f3d65140122047a919
Vivi Huang <rwhuang@thoughtworks.com>
2017-02-22 17:15:54 +0800

    Add get method for test api.

---
+++ entity/application.js
@@ -0,0 +1,4 @@
+module.exports = {
+  id: 0,
+  content: 'test'
+}
---
+++ resources/test/get.js
@@ -0,0 +1,6 @@
+import application from '../../entity/application'
+
+module.exports = (req, res, next) => {
+  res.send(200, application)
+  next()
+}
