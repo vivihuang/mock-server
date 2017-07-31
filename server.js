0fc4f073a9ee5bd508b35b08462e0aa120672bca
Vivi Huang <rwhuang@thoughtworks.com>
2017-07-31 11:02:54 +0800

    Add cors plugin to server config.

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import { addPath } from 'app-module-path'
 import restify from 'restify'
+import corsMiddleware from 'restify-cors-middleware'
+
 import routes from './routes'
 
 addPath(__dirname)
+
+const cors = corsMiddleware({
+  origins: ['*']
+})
 
 const server = restify.createServer({
   name: 'mock-server',
@@ -15,6 +21,8 @@ server.use(restify.plugins.bodyParser())
 
 server.use(restify.plugins.authorizationParser())
 server.use(restify.plugins.dateParser())
+server.use(cors.actual)
+server.pre(cors.preflight)
 server.pre(restify.pre.sanitizePath())
 server.pre((req, res, next) => {
   res.charSet('utf-8')
