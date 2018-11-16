ee0baae8197abcc9ae46129b04eefc395bc35b34
Vivi Huang <rwhuang@thoughtworks.com>
2018-11-16 10:47:52 +0800

    Fix error response.

--- resources/api/training-datasets/name-check/{name}/get.js
+++ resources/api/training-datasets/name-check/{name}/get.js
@@ -2,10 +2,9 @@ import { random } from 'lodash';
 
 module.exports = (req, res, next) => {
   const { params: { name } } = req;
-  const statusCode = random(0, 1) ? 200 : 409;
+  const statusCode = random(0, 1) ? 200 : 400;
   const errors =  [{
-    field: "name",
-    code: random(0, 1) ? "FormatInvalid" : "Duplicated",
+    name: "Duplicated",
   }]
   const body = statusCode === 200 ? { name } : { errors }
   res.send(statusCode, body);
